43471c20248cdb58ddbfef2dd6e181d9b93fe478
Daniel Palacios <dptorri@gmail.com>
2017-12-18 15:15:00 +0100

    article comp. up and downvote without F5

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+
 
 @Component({
   selector: 'app-article',
@@ -6,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+@HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+   }
+
+   voteUp() {
+     this.votes +=1;
+     return false;
+   }
+   voteDown() {
+    this.votes -=1;
+    return false;
+  }
 
   ngOnInit() {
   }
